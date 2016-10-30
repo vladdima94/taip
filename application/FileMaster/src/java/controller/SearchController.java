8016258d2c738a032fbeda4534d912de0552f708
vladdima94 <vlad.dima@info.uaic.ro>
2016-10-30 20:03:07 +0200

    Errors correction

--- application/FileMaster/src/java/controller/SearchController.java
+++ application/FileMaster/src/java/controller/SearchController.java
@@ -5,10 +5,27 @@
  */
 package controller;
 
+import SearchSystem.SearchAlgorithmFactory;
+import SearchSystem.SearchingAlgorithms.SearchAlgorithm;
+import dao.SearchSystemDAO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import utils.UriUtils;
+
 /**
  *
  * @author Vlad
  */
-public class SearchController {
+public class SearchController implements Controller{
+
+    @Override
+    public void processRequest(HttpServletRequest request, HttpServletResponse response, UriUtils uri) {
+        String algorithm = request.getParameter("algorithm");
+        //TODO: read double [] vectors from request and search
+        double [] data = null;
+        
+        SearchAlgorithm searchAlg = SearchAlgorithmFactory.getInstance().getAlgorithm(algorithm);
+        new SearchSystemDAO().searchFiles(10, searchAlg);
+    }
     
 }
