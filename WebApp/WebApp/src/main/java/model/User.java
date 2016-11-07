c9d0be00110c44de783e84fd719a69acdf05e140
BogdanA <Bogdan.Aciobanitei@info.uai.ro>
2016-11-07 18:13:14 +0200

    WebApp roots

---
+++ WebApp/WebApp/src/main/java/model/User.java
@@ -0,0 +1,87 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import base.BaseEntity;
+
+@Entity
+@Table(name = "User")
+public class User extends BaseEntity{
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "email", nullable = true, unique = true)
+	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+}
---
+++ WebApp/WebApp/src/main/java/service/IUserService.java
@@ -0,0 +1,8 @@
+package service;
+
+import model.User;
+import base.AbstractService;
+
+public interface IUserService extends AbstractService<User>{
+
+}
